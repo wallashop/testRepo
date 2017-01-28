26bc23f4923a83d781fd8b30909d75bf11cc8c41
Oleg verhovsky <verchol@gmail.com>
2017-01-28 22:34:08 +0200

    fix

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' , version : (process.env.VERSION||'0.0.0.1')});
+});
+router.get('/env', function(req, res) {
+  res.render('env', {  env: JSON.stringify(process.env)}); //env support
+});
+
+module.exports = router;
