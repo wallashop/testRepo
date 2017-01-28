464b8119103045f3a1816168f7bc490228c7b22a
Oleg verhovsky <verchol@gmail.com>
2017-01-28 22:42:23 +0200

    add version

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' , version : (process.env.VERSION||'0.0.0.1')});
+  res.render('index', { title: 'Express' , version : (process.env.VERSION||'0.0.0.2')});
 });
 router.get('/env', function(req, res) {
   res.render('env', {  env: JSON.stringify(process.env)}); //env support
